ccd6f1ebe7deb78114b82f04c632226b4d6d8e2a
srikanthbusi <srikanthbusi3@gmail.com>
2024-03-27 19:21:47 +0530

    yes

--- day1/code8.py
+++ day1/code8.py
@@ -22,3 +22,17 @@ for i in range(2,n):
     i=j
     j=sum
     sum=i+j
+
+
+# printing the Fibanocci  series in list
+n = int(input())
+i =0
+j=1
+list1 = [0,1]
+for num in range(2,n):
+    k = i+j
+    list1.append(k)
+    i=j
+    j=k
+print(list1)    
+    

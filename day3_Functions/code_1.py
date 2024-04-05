98602e2f32fb232c7eed38671b57d7b5aa279a95
srikanthbusi <srikanthbusi3@gmail.com>
2024-04-05 10:59:51 +0530

    commit

--- day3_Functions/code_1.py
+++ day3_Functions/code_1.py
@@ -3,32 +3,54 @@
 # then multiply both the arguments and print the last digit of the result
 # def check(a,b):
 #     b = int(b)
-#     print(a*b)
-#     print((a*b)%10)
+#     print("Multiplication of two numbers after typecasting b: ",a*b)
+#     print("The last number is",(a*b)%10)
 
 # check(4,6.56)  #postion argument 
 # check(b=4,a=6.56)   #keyword args
 
 
 
-# # default args
+# # # default arguments
 # def city(name="srikanth",city="mandapeta"):
-#     print("my name is",name "and the city is",city)
+#     print(f"My name is {name} and the city is {city}")
+
 
 # city()
 # city("justin","carlifornia")
 
 
-def identity(area,pincode,rating,place="none",domicile="karachi",):
-    area = pincode*(rating)
+# def identity(area,pincode,rating,place="none",domicile="karachi",):
+#     area = pincode*(rating)
     # print("my name is",place "and the city is",domicile)
 
-    print(area)
-identity(4556,5,56)
-identity()
-identity(fname = "srikanth",lname = "beiber")
+#     print(area)
+# identity(4556,5,56)
+# identity()
+# identity(fname = "srikanth",lname = "beiber")
 
-def identity(**name):  #unkown parameters
-    print("My name is::",name["fname"])
+# def identity(**name):  #unkown parameters
+#     print("My name is::",name["fname"])
 
-identity(fname = "srikanth",lname = "beiber")
+# identity(fname = "srikanth",lname = "beiber")
+
+
+# Arbitrary Arguments/ Variable Length arguments
+# def webseries(*series):
+#     print("\nThe recommended Web series are:",end=" ")
+#     for i in series:
+#         print(i,end =",")
+
+# webseries("Dark","Modernfamily","FamilyMan","Save The Tigers")
+
+# Arbitrary keyword Arguments
+# def sports(**persons):
+#     print("\nName: " +persons["name"])
+#     print("Sports category: "+persons["category"])
+#     print("Level of Execelence in Career: " +persons["Level"])
+
+# sports(name = "Sachin Tendulkar",category = "Cricket", Level = "Legend")
+# sports(name = "MS Dhoni",category = "Cricket", Level = "Legend")
+# sports(name = "Mary kom",category = "Boxing", Level = "Legend")
+# sports(name = "Bajrang punia",category = "Boxing", Level = "Novice")
+
